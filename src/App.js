b7b714a4c32045b0ba1e3034504245d1baa586f2
Zana Flander <zana.flander@kiwi.com>
2019-04-28 19:40:44 +0200

    Add: simple UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { store } from './store';
 
-function App() {
+const FilterLink = ({filter, currentFilter, children}) => {
+  if (filter === currentFilter) {
+    return <span>{children}</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a href='#'
+       onClick={e => {
+         e.preventDefault();
+         store.dispatch({
+           type: 'SET_VISIBILITY_FILTER',
+           filter
+         });
+       }}
+    >
+      {children}
+    </a>
   );
+};
+
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(
+        t => t.completed
+      );
+    case 'SHOW_ACTIVE':
+      return todos.filter(
+        t => !t.completed
+      );
+    default:
+      return null // should not happen
+  }
+};
+
+let nextTodoId = 0;
+
+class App extends Component {
+  render() {
+    const todos = store.getState().todos;
+    const visibilityFilter = store.getState().visibilityFilter;
+    const visibleTodos = getVisibleTodos(todos, visibilityFilter);
+
+    return (
+      <div className="App">
+        <input ref={node => {
+          this.input = node;
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          // after action is dispatched, the field is cleared
+          this.input.value = '';
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {visibleTodos.map(todo =>
+            <li key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                });
+              }}
+              style={{
+                textDecoration:
+                todo.completed ?
+                'line-through' :
+                'none'
+              }}>
+                {todo.text}
+            </li>
+          )}
+        </ul>
+        <p>
+          Show:
+          {' '}
+          <FilterLink
+            filter='SHOW_ALL'
+            currentFilter={visibilityFilter}
+          >
+            All
+          </FilterLink>
+          {', '}
+          <FilterLink
+            filter='SHOW_ACTIVE'
+            currentFilter={visibilityFilter}
+          >
+            Active
+          </FilterLink>
+          {', '}
+          <FilterLink
+            filter='SHOW_COMPLETED'
+            currentFilter={visibilityFilter}
+          >
+            Completed
+          </FilterLink>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -3,44 +3,44 @@ import {todoApp} from "../reducers";
 
 export const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 0,
-  text: 'Learn Redux'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 1,
-  text: 'Go shopping'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching TOGGLE_TODO.');
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_COMPLETED'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
+// console.log('Initial state:');
+// console.log(store.getState());
+// console.log('--------------');
+//
+// console.log('Dispatching ADD_TODO.');
+// store.dispatch({
+//   type: 'ADD_TODO',
+//   id: 0,
+//   text: 'Learn Redux'
+// });
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('--------------');
+//
+// console.log('Dispatching ADD_TODO.');
+// store.dispatch({
+//   type: 'ADD_TODO',
+//   id: 1,
+//   text: 'Go shopping'
+// });
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('--------------');
+//
+// console.log('Dispatching TOGGLE_TODO.');
+// store.dispatch({
+//   type: 'TOGGLE_TODO',
+//   id: 0
+// });
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('--------------');
+//
+// console.log('Dispatching SET_VISIBILITY_FILTER');
+// store.dispatch({
+//   type: 'SET_VISIBILITY_FILTER',
+//   filter: 'SHOW_COMPLETED'
+// });
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('--------------');
